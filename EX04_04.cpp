6e61011a61add2e798987f2bfbbd0ccfbe55c072
jrobinson-mcclure19 <jrobinson-mcclure19@my.whitworth.edu>
2016-10-25 20:02:34 -0700

    Add files via upload

---
+++ EX04_04.cpp
@@ -0,0 +1,19 @@
+////////////////////////
+// Jalen Robinson-McClure
+// CS 172-1
+// October 25th, 2016
+// EX_04_04
+/////////////////////////
+
+#include <iostream>
+#include "Rectangle2D.h"
+
+using namespace std;
+
+int main()
+{
+	Rectangle2D rectangle1;
+	cout << "The area of the rectangle is " << rectangle1.getArea() << ". The perimeter of the rectangle is " << rectangle1.getPerimeter() << endl;
+
+	return 0;
+}
